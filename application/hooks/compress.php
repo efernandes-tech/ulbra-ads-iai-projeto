df9e76d734ef8279d5b49138f7e6877b633d4e08
edersonlrf <edersonluis.rf@gmail.com>
2018-11-15 22:02:24 -0200

    comprimindo html de saida com um hook

---
+++ application/hooks/compress.php
@@ -0,0 +1,41 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function compress() {
+    ini_set("pcre.recursion_limit", "16777");
+    $CI = & get_instance();
+    $buffer = $CI->output->get_output();
+    $re = '%
+        (?>
+            [^\S ]\s*
+        | \s{2,}
+        )
+        (?=
+            [^<]*+
+            (?:
+                <
+                (?!/?(?:textarea|pre|script)\b)
+                [^<]*+
+            )*+
+            (?:
+                <
+                (?>textarea|pre|script)\b
+            | \z
+            )
+        )
+        %Six';
+    
+    $new_buffer = preg_replace($re, " ", $buffer);
+    
+    if ($new_buffer === null) {
+        $new_buffer = $buffer;
+    }
+    
+    $CI->output->set_output($new_buffer);
+    $CI->output->_display();
+}
