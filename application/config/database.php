a14128dfdde6914d39af41d3fb30d4fa456c1681
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-11-18 21:05:56 -0200

    update

--- application/config/database.php
+++ application/config/database.php
@@ -121,10 +121,10 @@ $db['development'] = array(
 
 $db['production'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'ulbra-ads-iai-projeto',
+	'hostname' => 'ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	'username' => 'hhr0zp11wmieazwx',
+	'password' => 'lr2medz2xi0abz9i',
+	'database' => 'ei9bfa0xc9tbzloa',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
