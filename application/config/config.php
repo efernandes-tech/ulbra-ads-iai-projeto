33e43f2e5b3b628562c841300d8021f4c08b39b9
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-11-18 21:12:05 -0200

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ulbra-ads-iai-projeto/';
+$config['base_url'] = MY_BASEURL;
 
 /*
 |--------------------------------------------------------------------------
